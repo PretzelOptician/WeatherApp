89ab8644db3da8e6fab3f3dbe2be940116eb163c
PretzelOptician <alekschultz1@gmail.com>
2022-05-23 14:14:06 -0400

    Updates

---
+++ precache-manifest.0ead6e9cb2d82ee7348e4f4c62cea214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "15c0ad21dc36525d52e2",
+    "url": "/WeatherApp/static/css/main.87990eeb.chunk.css"
+  },
+  {
+    "revision": "15c0ad21dc36525d52e2",
+    "url": "/WeatherApp/static/js/main.15c0ad21.chunk.js"
+  },
+  {
+    "revision": "128fd6128b9459c5ab86",
+    "url": "/WeatherApp/static/js/1.128fd612.chunk.js"
+  },
+  {
+    "revision": "4dc7695114457c402648",
+    "url": "/WeatherApp/static/js/2.4dc76951.chunk.js"
+  },
+  {
+    "revision": "360d3f5c515db4be2c5a",
+    "url": "/WeatherApp/static/js/runtime~main.360d3f5c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/WeatherApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "378336215963bb58391a905c10abf010",
+    "url": "/WeatherApp/index.html"
+  }
+];
